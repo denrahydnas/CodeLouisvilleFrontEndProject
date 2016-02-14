030042a4f3b386a1f294a60b21f17ced832c9fe7
Denrahydnas <sandylin34@gmail.com>
2016-02-14 17:34:33 -0500

    interactive nav

---
+++ js/navigation.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+$('ul').hide();
+
+$('.nav').mouseenter(function(){
+    $('.nav').css('background', "#FFF");
+    $('.nav').css('box-shadow', '-5px 5px 10px #FC3');
+});
+    
+$('.nav').mouseleave(function(){
+    $('.nav').css('background', "rgba(255,255,255,0.9)")
+    $('.nav').css('box-shadow', '-5px 5px 10px #006')
+});
+    
+$(".nav").click(function(){
+    $("ul").slideToggle();
+});  
+      
+});
