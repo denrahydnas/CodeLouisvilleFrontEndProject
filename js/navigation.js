2262b57e7efab3fe08d2659ca7a1446a65f7bdfe
Denrahydnas <sandylin34@gmail.com>
2016-02-27 17:44:12 -0500

    starting bootstrap and slick implementation

--- js/navigation.js
+++ js/navigation.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
 
-$('.nav ul').hide();
+$('nav ul').hide();
 
-$('.nav').mouseenter(function(){
-    $('.nav').css('background', "#FFF");
-    $('.nav').css('box-shadow', '-5px 5px 10px #FC3');
+$('nav').mouseenter(function(){
+    $('nav').css('background', "#FFF");
+    $('nav').css('box-shadow', '-5px 5px 10px #FC3');
 });
     
-$('.nav').mouseleave(function(){
-    $('.nav').css('background', "rgba(255,255,255,0.9)")
-    $('.nav').css('box-shadow', '-5px 5px 10px #006')
+$('nav').mouseleave(function(){
+    $('nav').css('background', "rgba(255,255,255,0.9)")
+    $('nav').css('box-shadow', '-5px 5px 10px #006')
 });
     
-$(".nav").click(function(){
-    $(".nav ul").slideToggle();
+$("nav").click(function(){
+    $("nav ul").slideToggle();
 });  
       
 });
---
+++ js/slickjq.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    
+ $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav',
+    speed: 600
+});
+$('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    focusOnSelect: true,
+    speed: 800
+});
+	  
+    
+});
+
+
+
